af8a6b02b77aeb134c6292c09a9b3af3a873ed1c
jiwoo <sgo8308@naver.com>
2022-01-18 22:01:04 +0900

    feat : create Point which has x,y and add of(), validation()

---
+++ src/main/java/coordinatecalculator/line/domain/Point.java
@@ -0,0 +1,27 @@
+package coordinatecalculator.line.domain;
+
+public class Point {
+
+    int x;
+    int y;
+
+    private Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point of(int x, int y) {
+        validation(x, y);
+        return new Point(x, y);
+    }
+
+    private static void validation(int x, int y) {
+        if (!isValid(x) || !isValid(y)) {
+            throw new IllegalArgumentException("좌표는 0이상 24이하만 입력 가능합니다.");
+        }
+    }
+
+    private static boolean isValid(int coordinate) {
+       return PointLimit.isInsideLimit(coordinate);
+    }
+}
