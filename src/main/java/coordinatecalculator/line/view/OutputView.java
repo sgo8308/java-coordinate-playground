f73f6f931653f6258f71efb8a5617d28526a8ca7
jiwoo <sgo8308@naver.com>
2022-01-20 01:00:56 +0900

    refactor : modify printInputMessage() to print message directly

--- src/main/java/coordinatecalculator/line/view/OutputView.java
+++ src/main/java/coordinatecalculator/line/view/OutputView.java
@@ -2,12 +2,13 @@ package coordinatecalculator.line.view;
 
 public class OutputView {
 
-    public static void printInputMessage(String inputMessage) {
-        System.out.println(inputMessage);
+    public static void printInputMessage() {
+        System.out.println("좌표를 입력하세요.");
     }
 
     public static void printBoard(String board) {
         System.out.println(board);
+        System.out.println(" ");
     }
 
     public static void printAttribute(String attibuteMessage) {
