1f56644f724a4002822da0a94248a8572c31cde5
jiwoo <sgo8308@naver.com>
2022-01-20 00:51:27 +0900

    feat : override equals() and hashcode() of Points

--- src/main/java/coordinatecalculator/line/domain/Points.java
+++ src/main/java/coordinatecalculator/line/domain/Points.java
@@ -2,6 +2,7 @@ package coordinatecalculator.line.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class Points {
 
@@ -19,5 +20,22 @@ public abstract class Points {
     public void markTo(String[][] board) {
         points.stream().map(Point::transform)
                 .forEach(transformedPoint -> transformedPoint.markTo(board));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Points points1 = (Points) o;
+        return Objects.equals(points, points1.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
     }
 }
