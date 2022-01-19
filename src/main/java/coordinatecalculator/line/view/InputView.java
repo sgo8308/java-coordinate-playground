23686f8a17580a158774a919bc56faa9572807b7
jiwoo <sgo8308@naver.com>
2022-01-19 21:12:33 +0900

    feat : create InputView and add inputCoordinate()

---
+++ src/main/java/coordinatecalculator/line/view/InputView.java
@@ -0,0 +1,12 @@
+package coordinatecalculator.line.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String inputCoordinate() {
+        return scanner.nextLine();
+    }
+}
