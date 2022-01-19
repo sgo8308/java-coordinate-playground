bd97dfc539946b4531278a8670e6d34201bc5a5a
jiwoo <sgo8308@naver.com>
2022-01-19 14:36:09 +0900

    feat : add calcultateDistance() to Point and refactor validation() to be more readable

--- src/main/java/coordinatecalculator/line/domain/Point.java
+++ src/main/java/coordinatecalculator/line/domain/Point.java
@@ -2,8 +2,8 @@ package coordinatecalculator.line.domain;
 
 public class Point {
 
-    int x;
-    int y;
+    private int x;
+    private int y;
 
     private Point(int x, int y) {
         this.x = x;
@@ -16,12 +16,19 @@ public class Point {
     }
 
     private static void validation(int x, int y) {
-        if (!isValid(x) || !isValid(y)) {
-            throw new IllegalArgumentException("좌표는 0이상 24이하만 입력 가능합니다.");
+        if (isValid(x) && isValid(y)) {
+            return;
         }
+        throw new IllegalArgumentException("좌표는 0이상 24이하만 입력 가능합니다.");
     }
 
     private static boolean isValid(int coordinate) {
-       return PointLimit.isInsideLimit(coordinate);
+        return PointLimit.isInsideLimit(coordinate);
+    }
+
+    public static double calculateDistance(Point point1, Point point2) {
+        return Math.sqrt(
+                Math.pow((point1.x - point2.x), 2) + Math.pow((point1.y - point2.y), 2)
+        );
     }
 }
