7f17ddb6b44ecc1fd2adf95d01583b5a792bd0f4
jiwoo <sgo8308@naver.com>
2022-01-19 21:13:21 +0900

    feat : create OutputView and add printInputMessage() and printBoard() and printAttribute()

---
+++ src/main/java/coordinatecalculator/line/view/OutputView.java
@@ -0,0 +1,16 @@
+package coordinatecalculator.line.view;
+
+public class OutputView {
+
+    public static void printInputMessage(String inputMessage) {
+        System.out.println(inputMessage);
+    }
+
+    public static void printBoard(String board) {
+        System.out.println(board);
+    }
+
+    public static void printAttribute(String attibuteMessage) {
+        System.out.println(attibuteMessage);
+    }
+}
