701a6248f2b1632a78fe8e7b82ae6acb38ec6478
jiwoo <sgo8308@naver.com>
2022-01-20 21:56:31 +0900

    test : add test of Square that verify input Coordinates form a rectangle

--- src/test/java/coordinatecalculator/line/domain/SquareTest.java
+++ src/test/java/coordinatecalculator/line/domain/SquareTest.java
@@ -1,6 +1,7 @@
 package coordinatecalculator.line.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,18 @@ class SquareTest {
 
         //then
         assertThat(actualMessage).hasToString("사각형 넓이는 96");
+    }
+
+    @Test
+    void Square_사각형이_아닌_포인트가_입력될_경우_예외를_던진다() {
+        assertThatThrownBy(() -> {
+            Figure square = Figure.of(
+                    Coordinate.of(10, 9),
+                    Coordinate.of(22, 10),
+                    Coordinate.of(22, 18),
+                    Coordinate.of(10, 18)
+            );
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("직사각형을 이루는 좌표만 입력 가능합니다.");
     }
 }
