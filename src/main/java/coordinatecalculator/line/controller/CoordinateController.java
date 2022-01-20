cb3cd38d81598aa1bf3ae5637fbd0be4b207a296
jiwoo <sgo8308@naver.com>
2022-01-20 14:34:26 +0900

    feat : add validation logic to CoordinateController main()

--- src/main/java/coordinatecalculator/line/controller/CoordinateController.java
+++ src/main/java/coordinatecalculator/line/controller/CoordinateController.java
@@ -8,12 +8,31 @@ import coordinatecalculator.line.view.OutputView;
 
 public class CoordinateController {
     public static void main(String[] args) {
-        OutputView.printInputMessage();
-        String input = InputView.inputCoordinate();
-        Points points = PointsParser.parsePoints(input);
+        Points points = inputUntilValidPoints();
         Board board = new Board();
         board.markPoints(points);
         OutputView.printBoard(board.toString());
         OutputView.printAttribute(points.getAttributeMessage());
+    }
+
+    private static Points inputUntilValidPoints() {
+        String input = "";
+        boolean isValid = false;
+        while (!isValid) {
+            OutputView.printInputMessage();
+            input = InputView.inputCoordinate();
+            isValid = isValid(input);
+        }
+        return PointsParser.parsePoints(input);
+    }
+
+    private static boolean isValid(String input) {
+        try {
+            PointsParser.isValid(input);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
