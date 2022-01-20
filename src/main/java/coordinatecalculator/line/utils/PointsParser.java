b4a9d2ec653d548190e9e546ac6960ba0c1c3fd3
jiwoo <sgo8308@naver.com>
2022-01-20 14:33:36 +0900

    feat : add isValid() and remove duplication of parsePoint() in PointsParser

--- src/main/java/coordinatecalculator/line/utils/PointsParser.java
+++ src/main/java/coordinatecalculator/line/utils/PointsParser.java
@@ -3,10 +3,12 @@ package coordinatecalculator.line.utils;
 import coordinatecalculator.line.domain.Point;
 import coordinatecalculator.line.domain.Points;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PointsParser {
+
     public static Points parsePoints(String input) {
         String[] inputs = input.split("-");
         ArrayList<Point> points = new ArrayList<>();
@@ -17,12 +19,23 @@ public class PointsParser {
         return Points.of(points.toArray(new Point[points.size()]));
     }
 
-    private static Point parsePoint(String inputs) {
-        Matcher matcher = Pattern.compile("[0-9]+").matcher(inputs);
-        matcher.find();
-        int x = Integer.parseInt(matcher.group());
-        matcher.find();
-        int y = Integer.parseInt(matcher.group());
-        return Point.of(x, y);
+    private static Point parsePoint(String coordinate) {
+        Matcher XYValueMatcher = Pattern.compile("[0-9]+").matcher(coordinate);
+        List<Integer> values = new ArrayList<>();
+        while (XYValueMatcher.find()) {
+            values.add(Integer.valueOf(XYValueMatcher.group()));
+        }
+        return Point.of(values.get(0), values.get(1));
+    }
+
+    public static void isValid(String input) {
+        inputValidation(input);
+        parsePoints(input);
+    }
+
+    private static void inputValidation(String input) {
+        if (!input.matches("^(\\([0-9]+,[0-9]+\\)-)*(\\([0-9]+,[0-9]+\\))$")) {
+            throw new IllegalArgumentException("정확한 형식을 지켜서 입력해주세요. ex (10,10)-(5,23)");
+        }
     }
 }
